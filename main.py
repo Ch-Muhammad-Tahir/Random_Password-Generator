37242bb90c01fab05ebc2a6c5a733be75bc38916
Ch-Muhammad-Tahir <tahir.ctn001@gmail.com>
2021-08-25 22:18:58 +0500

    Add Symobls

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'g', 'i', 'j', 'k', 'l', 'm', 'n',
            'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 numberS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+','@','?']
 print("Welcome To PyPassword Generator")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
